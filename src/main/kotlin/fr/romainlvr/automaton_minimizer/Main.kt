bee1f4248306c7740fae0022dc8b0769381639c8
RomainLvr <lecouvreurromain@gmail.com>
2023-03-18 15:02:07 +0100

    ✨ Create .dot parser (read file only)

--- src/main/kotlin/fr/romainlvr/automaton_minimizer/Main.kt
+++ src/main/kotlin/fr/romainlvr/automaton_minimizer/Main.kt
@@ -1,5 +1,9 @@
 package fr.romainlvr.automaton_minimizer
 
+import fr.romainlvr.automaton_minimizer.parser.ParseDot
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    val parser = ParseDot()
+    parser.parseFile("automaton.dot")
 }
---
+++ src/main/kotlin/fr/romainlvr/automaton_minimizer/parser/ParseDot.kt
@@ -0,0 +1,31 @@
+package fr.romainlvr.automaton_minimizer.parser
+
+import java.io.File
+import java.io.FileNotFoundException
+import com.google.common.io.Resources
+
+
+class ParseDot{
+
+    private var file: File = File("")
+
+    fun parseFile(path: String){
+        openFile(path)
+        readFile()
+    }
+    private fun openFile(path: String){
+
+        try {
+            this.file = File(Resources.getResource(path).file)
+        }catch (e: FileNotFoundException){
+            println("File not found :" + e.message)
+        }
+
+    }
+
+    private fun readFile(){
+        this.file.forEachLine {
+            println(it)
+        }
+    }
+}
